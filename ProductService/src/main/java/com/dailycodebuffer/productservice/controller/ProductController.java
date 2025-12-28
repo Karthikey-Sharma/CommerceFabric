047f62d50e1316130f2479d1ca1cce98b7d03d77
Karthikey-Sharma <karthikeysharma22@gmail.com>
2025-12-29 02:45:39 +0530

    Implemented Get Product API

--- ProductService/src/main/java/com/dailycodebuffer/productservice/controller/ProductController.java
+++ ProductService/src/main/java/com/dailycodebuffer/productservice/controller/ProductController.java
@@ -1,14 +1,12 @@
 package com.dailycodebuffer.productservice.controller;
 
 import com.dailycodebuffer.productservice.model.ProductRequest;
+import com.dailycodebuffer.productservice.model.ProductResponse;
 import com.dailycodebuffer.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -22,7 +20,11 @@ public class ProductController {
         return new ResponseEntity<>(productId , HttpStatus.CREATED);
     }
 
-    public ResponseEntity<>
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId){
+        ProductResponse productResponse = productService.getProductById(productId);
+        return new ResponseEntity<>(productResponse , HttpStatus.OK);
+    }
 
 
 }
--- ProductService/src/main/java/com/dailycodebuffer/productservice/service/ProductServiceImpl.java
+++ ProductService/src/main/java/com/dailycodebuffer/productservice/service/ProductServiceImpl.java
@@ -2,10 +2,14 @@ package com.dailycodebuffer.productservice.service;
 
 import com.dailycodebuffer.productservice.entity.Product;
 import com.dailycodebuffer.productservice.model.ProductRequest;
+import com.dailycodebuffer.productservice.model.ProductResponse;
 import com.dailycodebuffer.productservice.repository.ProductRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.beans.BeanUtils.*;
 
 @Service
 @Log4j2
@@ -25,5 +29,17 @@ public class ProductServiceImpl implements ProductService{
 
         log.info("Product Created");
         return product.getProductId();
+    }
+
+    @Override
+    public ProductResponse getProductById(long productId) {
+        log.info("Get the Product for productId : {}" , productId);
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Product with given id not found"));
+
+        ProductResponse productResponse = new ProductResponse();
+        copyProperties(product , productResponse);
+        return productResponse;
+
     }
 }
