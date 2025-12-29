3bb635450c5429213f72234874891a7b4af59dc9
Karthikey-Sharma <karthikeysharma22@gmail.com>
2025-12-29 20:53:45 +0530

    Implemented reduce quantity API in product

--- ProductService/src/main/java/com/dailycodebuffer/productservice/service/ProductService.java
+++ ProductService/src/main/java/com/dailycodebuffer/productservice/service/ProductService.java
@@ -7,4 +7,6 @@ public interface ProductService {
     long addProduct(ProductRequest productRequest);
 
     ProductResponse getProductById(long productId);
+
+    void reduceQuantity(Long productId, Long quantity);
 }
