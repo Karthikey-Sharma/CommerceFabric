78d1bb774b601fcc1aa94dfc4c0e4d9ba3730c27
Karthikey-Sharma <karthikeysharma22@gmail.com>
2025-12-29 18:40:25 +0530

    Create place order API with Basic Data Saving

--- OrderService/src/main/java/com/dailycodebuffer/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/dailycodebuffer/orderservice/controller/OrderController.java
@@ -1,8 +1,13 @@
 package com.dailycodebuffer.orderservice.controller;
 
+import com.dailycodebuffer.orderservice.model.OrderRequest;
 import com.dailycodebuffer.orderservice.service.OrderService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 @Log4j2
 public class OrderController {
-    
+    @Autowired
+    private OrderService orderService;
+
+    @PostMapping("/placeOrder")
+    public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest){
+        long orderId = orderService.placeOrder(orderRequest);
+        log.info("Order ID: {}" , orderId);
+        return new ResponseEntity<>(orderId ,HttpStatus.OK);
+    }
 }
--- OrderService/src/main/java/com/dailycodebuffer/orderservice/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/dailycodebuffer/orderservice/service/OrderServiceImpl.java
@@ -1,9 +1,37 @@
 package com.dailycodebuffer.orderservice.service;
 
+import com.dailycodebuffer.orderservice.entity.Order;
+import com.dailycodebuffer.orderservice.model.OrderRequest;
+import com.dailycodebuffer.orderservice.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @Service
 @Log4j2
 public class OrderServiceImpl implements OrderService{
+    @Autowired
+    private OrderRepository orderRepository;
+    @Override
+    public long placeOrder(OrderRequest orderRequest) {
+        // OrderEntity -> save the data with status order Created
+        // Product Service -> Block Products (Reduce the Quantity)
+        // PaymentService -? Complete the payment -> SUCESS -> complete or cancelled
+
+        log.info("Placing Order request : {}" , orderRequest);
+        Order order = Order.builder()
+                .amount(orderRequest.getTotalAmount())
+                .orderStatus("CREATED")
+                .productId(orderRequest.getProductId())
+                .orderDate(Instant.now())
+                .quantity(orderRequest.getQuantity())
+                .build();
+
+        order = orderRepository.save(order);
+        log.info("Order Placed successfully with  Order Id: {}" , order.getId());
+        return order.getId();
+
+  }
 }
